c434725013c89680ab0113a9efe37af93d915c89
Figo57 <ambmc57@gmail.com>
2023-11-05 02:25:54 +0800

    1.97%

--- chatgpt_next_web.rb
+++ chatgpt_next_web.rb
@@ -2,7 +2,7 @@ class ChatgptNextWeb < Formula
   desc "GPT-Assets"
   homepage "https://github.com/Figo57/GPT-Assets"
   url "https://github.com/Yidadaa/ChatGPT-Next-Web/releases/download/v2.9.9/ChatGPT.Next.Web_x64.app.tar.gz"
-  sha256 "113a17f5f9a72a412b1c6372e239602e19528bd97ebc47647669a76448323ed3"
+  sha256 "31a51a8dbf922c424ff0d4d9e19b25931cef74c5b0dd1e1ed7e4e67c20e8b6f1"
 
 def install
   bin.install "ChatGPT.Next.Web_x64.app.tar.gz"
