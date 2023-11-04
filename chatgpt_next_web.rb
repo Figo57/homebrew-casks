162894a0084e64ee521041001d9227fe1ee7d349
Figo57 <ambmc57@gmail.com>
2023-11-05 00:19:13 +0800

    1.4%

--- chatgpt_next_web.rb
+++ chatgpt_next_web.rb
@@ -5,7 +5,7 @@ class ChatgptNextWeb < Formula
   sha256 "113a17f5f9a72a412b1c6372e239602e19528bd97ebc47647669a76448323ed3"
 
   def install
-    system "tar", "-xzf", "ChatGPT.Next.Web_x64.app.tar.gz"
+    system "tar", "-xzf", "#{HOMEBREW_CACHE}/ChatGPT.Next.Web_x64.app.tar.gz"
     bin.install "ChatGPT.Next.Web_x64.app" => "chatgpt_next_web"
   end
 
