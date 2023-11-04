9fd807950a5906a56bf385427104aa5b39e59f7e
Figo57 <ambmc57@gmail.com>
2023-11-05 00:56:46 +0800

    1.8%

--- chatgpt_next_web.rb
+++ chatgpt_next_web.rb
@@ -1,11 +1,13 @@
-class ChatgptNextWeb < Formulae
+class ChatgptNextWeb < Formula
   desc "GPT-Assets"
   homepage "https://github.com/Figo57/GPT-Assets"
   url "https://github.com/Yidadaa/ChatGPT-Next-Web/releases/download/v2.9.9/ChatGPT.Next.Web_x64.app.tar.gz"
   sha256 "113a17f5f9a72a412b1c6372e239602e19528bd97ebc47647669a76448323ed3"
 
   def install
-    system "gunzip", "#{HOMEBREW_CACHE}/ChatGPT.Next.Web_x64.app.tar.gz"
+    # Use buildpath to get the correct file path
+    system "gunzip", buildpath/"ChatGPT.Next.Web_x64.app.tar.gz"
+    # This assumes the binary is in the current directory and named "ChatGPT.Next.Web_x64.app"
     bin.install "ChatGPT.Next.Web_x64.app" => "chatgpt_next_web"
   end
 
