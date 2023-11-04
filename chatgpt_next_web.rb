a378a082a4247bdb1cbc5cc89be3ee6f1d16c4a0
Figo57 <ambmc57@gmail.com>
2023-11-05 00:08:57 +0800

    1.2%

--- chatgpt_next_web.rb
+++ chatgpt_next_web.rb
@@ -1,15 +1,14 @@
 class ChatgptNextWeb < Formula
   desc "GPT-Assets"
-  homepage "https://github.com/Figo57/GPT-Assets"  # Replace with your project's URL
+  homepage "https://github.com/Figo57/GPT-Assets"
   url "https://github.com/Yidadaa/ChatGPT-Next-Web/releases/download/v2.9.9/ChatGPT.Next.Web_x64.app.tar.gz"
-  sha256 "113a17f5f9a72a412b1c6372e239602e19528bd97ebc47647669a76448323ed3"  # Replace with the actual SHA-256
+  sha256 "113a17f5f9a72a412b1c6372e239602e19528bd97ebc47647669a76448323ed3"
 
   def install
-    system "tar", "-xzf", "ChatGPT.Next.Web_x64.app.tar.gz"  # Unzip the tar.gz file
-    bin.install "ChatGPT.Next.Web_x64.app"  # Now install the binary
+    bin.install "ChatGPT.Next.Web_x64.app" => "chatgpt_next_web"
   end
 
   test do
-    system "false"
+    system "#{bin}/chatgpt_next_web", "--version"
   end
 end
