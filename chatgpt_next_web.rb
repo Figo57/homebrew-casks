8a1711d3375efe7a4822fe0214f8b925abaf2c82
Figo57 <ambmc57@gmail.com>
2023-11-05 01:34:11 +0800

    1.9%

--- chatgpt_next_web.rb
+++ chatgpt_next_web.rb
@@ -5,10 +5,8 @@ class ChatgptNextWeb < Formula
   sha256 "113a17f5f9a72a412b1c6372e239602e19528bd97ebc47647669a76448323ed3"
 
   def install
-    # Use buildpath to get the correct file path
-    system "gunzip", buildpath/"ChatGPT.Next.Web_x64.app.tar.gz"
-    # This assumes the binary is in the current directory and named "ChatGPT.Next.Web_x64.app"
-    bin.install "ChatGPT.Next.Web_x64.app" => "chatgpt_next_web"
+    # assuming the app is already present in current directory (the buildpath)
+    prefix.install "ChatGPT Next Web.app"
   end
 
   test do
